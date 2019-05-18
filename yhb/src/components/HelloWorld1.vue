c04fcb0c2ba562ee5865dc241d77ee5d2c8d7b28
qs-mszq <1375388499@qq.com>
2019-05-18 14:09:54 +0800

    vue

---
+++ yhb/src/components/HelloWorld1.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    页面2
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  padding: 0px 10px;
+}
+</style>
